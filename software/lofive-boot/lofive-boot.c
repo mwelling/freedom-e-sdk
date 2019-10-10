b7841bcb6660a6cb13b11ff0bd9932e934ee6b6c
Michael Welling <mwelling@ieee.org>
2019-10-10 15:52:46 -0700

    Remove build artifacts
    Signed-off-by: Michael Welling <mwelling@ieee.org>

--- software/lofive-boot/lofive-boot.c
+++ software/lofive-boot/lofive-boot.c
@@ -5,10 +5,11 @@
 #include <metal/init.h>
 
 #define FINAL_ADDRESS 0x20400000
+#define BOOTLOADER_VERSION "v0.0"
 
 int main()
 {
-    printf("LoFive Bootloader (%s) \n");
+    printf("LoFive Bootloader (%s) \n", BOOTLOADER_VERSION);
     printf("Jumping to 0x%x\n", FINAL_ADDRESS);
 
     void (*pgm_start)(void) = (void*) FINAL_ADDRESS;
